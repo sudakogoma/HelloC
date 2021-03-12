33ab0ab6e305bf3175fd61fc47f430c889ac4109
sudakogoma <sudako.goma@gmail.com>
2021-03-13 06:26:30 +0900

    Add test code

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
+int getA()
+{
+    return 2;
+}
+
+int getRoundValue(int getValue(), int min, int max)
+{
+    int x = (*getValue)();
+    if (x < min)
+    {
+        x = min;
+    }
+    if (x > max)
+    {
+        x = max;
+    }
+    return x;
+}
+
 int main(void)
 {
     printf("hello world.\n");
+
+    int a = 0;
+    int b = 1;
+
+    a |= (b == 1) << 1;
+
+    int c = getRoundValue(&getA, -1, 1);
+    char* chr;
+    sprintf(chr, "%d", c);
+    printf("%s\n", chr);
+
     return 0;
 }
